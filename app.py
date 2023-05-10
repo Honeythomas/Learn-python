99fe8ca1c599a10367d8abc7cac2c330d30b5399
Honeythomas <117334674+Honeythomas@users.noreply.github.com>
2023-05-10 09:59:48 +0530

    input function

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-age = 20
-price = 100000
-first_name = "Honey"
-is_online = True
-print(is_online, age, price, first_name)
+# age = 20
+# price = 100000
+# first_name = "Honey"
+# is_online = True
+# print(is_online, age, price, first_name)
+
+name = input("What is your name? ")
+print("Hello " + name )
